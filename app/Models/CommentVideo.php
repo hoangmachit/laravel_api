877e442e6c5e83215e170469dd4bc34f534a0dde
Hoang Mach <hoangmach.it@gmail.com>
2022-10-05 23:00:40 +0700

    Initial commit

---
+++ app/Models/CommentVideo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommentVideo extends Model
+{
+    use HasFactory;
+    protected $table = 'table_comment_video';
+}
---
+++ app/Models/ProductSub.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSub extends Model
+{
+    use HasFactory;
+    protected $table = 'table_product_sub';
+}
